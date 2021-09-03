8d8bf7586d84ec8de3da0666e757c164063ae20e
Pratyushg26 <90026135+Pratyushg26@users.noreply.github.com>
2021-09-03 18:47:04 +0530

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,16 +1,16 @@
-Height=float(input("Enter your height in centimeters: "))
-Weight=float(input("Enter your Weight in Kg: "))
-Height = Height/100
-BMI=Weight/(Height*Height)
-print("your Body Mass Index is: ",BMI)
+height=float(input("Enter your height in centimeters: "))
+weight=float(input("Enter your Weight in Kg: "))
+height = height/100
+BMI=weight/(height*height)
+print("Body Mass Index is: ",BMI)
 if(BMI>0):
 	if(BMI<=16):
-		print("you are severely underweight")
+		print("Severely underweight")
 	elif(BMI<=18.5):
-		print("you are underweight")
+		print("Underweight")
 	elif(BMI<=25):
-		print("you are Healthy")
+		print("Healthy")
 	elif(BMI<=30):
-		print("you are overweight")
-	else: print("you are severely overweight")
-else:("enter valid details")
+		print("Overweight")
+	else: print("Severely overweight")
+else:("Please enter valid details")
